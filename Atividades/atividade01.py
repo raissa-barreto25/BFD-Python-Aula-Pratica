96a063f8e3aeedb00d8b21e35ed7a541bd275161
Raissa Ruana <rrbsiilva@gmail.com>
2025-08-08 00:08:48 +0000

    Atividades

---
+++ Atividades/atividade01.py
@@ -0,0 +1,19 @@
+print("Olá, tudo bem?")
+print("Meu nome é Raíssa.")
+print("Hoje é quinta-feira!")
+
+print("\nNome:João" "\nIdade:20 anos" "\nCurso:Python Básico" )
+
+nome = "João"
+idade = 20
+curso = "Python Básico"
+
+print(f"\nMe chamo {nome}, tenho {idade} anos e estou cursando {curso}.")
+
+print("\n####" "\n#  #" "\n#  #" "\n####")
+
+a = 10
+b = 5
+
+print("Soma:", a+b, "\nSubtração: ", a-b, "\nmultiplicação", a*b)
+print("Divisão: ", a/b, "\nDivisão Inteita: ", a//b, "\nResto da divisão: ", a%b, "Potência: ", a**b)
---
+++ aulas/aula02.py
@@ -0,0 +1,18 @@
+'''
+soma +
+subtracao - 
+mult *
+expo **
+divisao /
+divisao int //
+modulo(resto) %
+
+Operadores logicos 
+
+igualdade ==
+diferente !=
+maior que >
+menor que <
+maior ou igual >=
+menor ou igual <=
+'''
