faccb4a1543d89fe7b2de442b3db3ff10e6c4b77
raissa-barreto25 <rrbsiilva@gmail.com>
2025-08-21 23:48:18 +0000

    dicionario

---
+++ aulas/aula06.py
@@ -0,0 +1,48 @@
+######### Dicionario #######
+# dicionario_vazio = {}
+# print(type(dicionario_vazio))
+# print(dicionario_vazio)
+
+# dicionario = {
+#     "id": 0,
+#     "Nome": "Raíssa",
+#     "Idade": 30,
+#     "Nacionalidade": "Brasileira",
+#     "Rico": False,
+#     "Filmes Favoritos" : "Harry Potter"
+# }
+
+#print(dicionario["Nome"])
+#print(dicionario.get("Nome"))
+#print(dicionario.items())
+#print(dicionario.keys())
+#print(dicionario.values.())
+#dicionario["Profissão"] = "Desenvolvedor"
+#dicionario["Nome"] = "Aurora"
+#dicionario.update({"Idade":31})
+#dicionario.update({"Carro": True})
+#dicionario.pop("Carro")
+#dicionario.popitem()
+#dicionario.clear()
+#del dicionario ["Rico"]
+#del dicionario
+#dicionario2 = dicionario.copy()
+#dicionario3 = dict(dicionario)
+# for itens in dicionario:
+#     print(itens)
+# for itens in dicionario.items():
+#     print(itens)
+# for itens in dicionario.values():
+#     print(itens)
+# for itens in dicionario.values():
+#     if itens == "Raíssa":
+#         print("Achamos o Meliante")   
+# print(dicionario)
+
+# nomes_funcionarios = ["Fred", "João", "Maria"]
+# dicionario_aleatorio = {
+#     "nomes": nomes_funcionarios,
+#     "função": ["Desenvolvedor", "Tech Lead", "PO"],
+# }
+
+# print(dicionario_aleatorio["nomes"][2])
