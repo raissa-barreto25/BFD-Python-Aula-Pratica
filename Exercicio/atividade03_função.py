0b335fc57d21a55a24f67f736c24b406a0ec3c30
raissa-barreto25 <rrbsiilva@gmail.com>
2025-09-04 13:13:14 +0000

    exercicio funçao

--- Exercicio/atividade03_função.py
+++ Exercicio/atividade03_função.py
@@ -45,3 +45,35 @@ dados_pessoais(nome="Raissa", idade=30, cidade="Recife")
 
 # Questão 08 
 
+def calculadora(x,y,oper):
+    def soma(num1,num2):
+        return num1 + num2
+    def subtracao(num1,num2):
+        return num1 - num2
+    def multi(num1,num2):
+        return num1 * num2
+    def divisao(num1,num2):
+        return num1 / num2
+    if oper == "soma":
+        return soma(x , y)
+    elif oper == "subtracao":
+        return subtracao(x , y)
+    elif oper == "multi":
+        return multi(x , y)
+    elif oper == "divisao":
+        return divisao(x , y)
+    else:
+        return "Operação invalida"
+print(calculadora(10,5, "subtracao"))
+
+# Questão 09
+def aplica_operacao(num1,num2,operacao):
+    return operacao(num1,num2)
+def soma(a,b): 
+    return a + b
+def multiplicacao(a,b):
+    return a * b
+resut_soma = aplica_operacao(3,7,soma)
+print(resut_soma)
+resut_multi = aplica_operacao(7,5,multiplicacao)
+print(resut_multi)
