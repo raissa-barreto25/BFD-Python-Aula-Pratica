dcbb417a3a9be8c6bab45ce07f2b589f376b497c
raissa-barreto25 <rrbsiilva@gmail.com>
2025-09-17 22:56:29 +0000

    Exercicio Herança

--- Exercicio/atividade07_herança.py
+++ Exercicio/atividade07_herança.py
@@ -1,23 +1,26 @@
 # Questão 01(Criar uma classe pessoa com suas caracteristicas e uma classe funcionario que herde pessoa).
-# class Pessoa:
-#     def __init__(self, nome, dn, endereco, cpf):
-#         self.nome = nome
-#         self.dn = dn
-#         self.endereco = endereco
-#         self.__cpf = cpf
+class Pessoa:
+    def __init__(self, nome, dn, endereco, cpf):
+        self.nome = nome
+        self.dn = dn
+        self.endereco = endereco
+        self.__cpf = cpf
 
-#     def __str__(self):
-#         return f"Nome: {self.nome}, Data de nascimento: {self.dn}, Endereço: {self.endereco}, CPF: {self.__cpf}."
+    def __str__(self):
+        return f"Nome: {self.nome}, Data de nascimento: {self.dn}, Endereço: {self.endereco}, CPF: {self.__cpf}."
 
-# class Funcionario(Pessoa):
-#     def __init__(self, nome, dn, endereco, cpf, cargo, salario):
-#         super().__init__(nome, dn, endereco, cpf)
-#         self.cargo = cargo
-#         self.salario = salario
+class Funcionario(Pessoa):
+    def __init__(self, nome, dn, endereco, cpf, cargo, salario):
+        super().__init__(nome, dn, endereco, cpf)
+        self.cargo = cargo
+        self.salario = salario
+
+    def __str__(self):
+        return f"{super().__str__()}, Cargo: {self.cargo}, Salario: {self.salario}"
 
     
-# funcionario1 = Funcionario("Raissa", "25/12/1994", "Rua A", 12345678910, "Estagiaria", 500)
-# print(funcionario1, funcionario1.cargo, funcionario1.salario)
+funcionario1 = Funcionario("Raissa", "25/12/1994", "Rua A", 12345678910, "Estagiaria", 500)
+print(funcionario1)
 
 # Questão 02 
 class Computador:
@@ -28,11 +31,15 @@ class Computador:
         self.departamento = departamento
     
     def __str__(self):
-        return f"Computador de modelo: {self.modelo}, processador: {self.processador}, memoria: {self.quantidade_de_memoria}, do departamento: {self.departamento}."
+        return f"Computador de modelo: {self.modelo}, processador: {self.processador}, memoria: {self.quantidade_de_memoria}, do departamento: {self.departamento}"
     
 class Placa(Computador):
     def __init__(self, modelo, processador, quantidade_de_memoria, departamento, placa_de_video):
         super().__init__(modelo, processador, quantidade_de_memoria, departamento)
         self.placa_de_video = placa_de_video
 
-comput = Placa("")
+    def __str__(self):
+        return f"{super().__str__()}, Placa de Video: {self.placa_de_video}"
+
+comput = Placa("HP 256R G9", "Intel® Core™ i3", "256GB SSD", "Notebook ", "ntel Iris Xe Graphics G7 (80 EUs)")
+print(comput)
