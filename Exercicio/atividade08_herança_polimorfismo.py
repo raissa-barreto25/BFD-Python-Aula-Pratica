0f677172da698cfad6b3b31fd81bb05ee82f48b2
raissa-barreto25 <rrbsiilva@gmail.com>
2025-09-25 21:22:56 +0000

    Exercicio Herança e Polimorfismo

--- Exercicio/atividade08_herança_polimorfismo.py
+++ Exercicio/atividade08_herança_polimorfismo.py
@@ -78,4 +78,42 @@
 # print(cliente1.saudacao())
 # print(f"Seu saldo é {cliente1.saldo}")
 
-# Questão 05
+# # Questão 05
+# class Funcionario(Usuario):
+#     def __init__(self, nome, email, funcao):
+#         super().__init__(nome, email)
+#         self.funcao = funcao
+    
+# class Gerente(Funcionario):
+#     def __init__(self, nome, email, funcao, setor):
+#         super().__init__(nome, email, funcao)
+#         self.setor = setor
+
+#     def __str__(self):
+#         return f"Olá, Gerente {self.nome},E-mail: {self.email}, Cargo: {self.funcao}, Setor: {self.setor}"
+
+# gerente_departamento = Gerente("Raissa", "rrbsiilva@gmail.com", "Gerente", "TI")
+# print(gerente_departamento)
+
+# Questão 06 e 07
+# class Autenticacao:
+#     def login(self):
+#         return "Usuario Autenticado com Sucesso."
+    
+#     def status(self):
+#         return "Status da Autenticação: OK"
+    
+# class Permissao:
+#     def verificar_permissao(self):
+#         return "Permissão Concedida"
+    
+#     def status(self):
+#         return "Status de Permissão: OK"
+    
+# class Administrador (Autenticacao, Permissao):
+#     ...
+
+# admin = Administrador()
+# admin.login()
+# admin.verificar_permissao()
+# print(Administrador.__mro__)
