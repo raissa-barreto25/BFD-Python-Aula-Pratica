a1cba9e7d5235d8d5e3c775b456f787ccdebee03
Raíssa Ruana <rrbsiilva@gmail.com>
2025-10-07 21:24:06 +0000

    Atividades

--- Exercicio/atividade09_lambda.py
+++ Exercicio/atividade09_lambda.py
@@ -13,7 +13,17 @@ num = [1, 2, 3, 4, 5]
 print(reduce(lambda x, y : x + y, num))
 
 # Questão 04
+frutas = ["uva", "banana", "maçã", "laranja"]
+print(sorted(frutas, key=lambda item: len(item) ))
 
 # Questão 05
 nome = ["ana", "pedro", "maria"]
-print(list(map(lambda item : item[0].upper, nome)))
+print(list(map(lambda item : item[:].title(), nome)))
+
+# Questão 06
+mult = [2, 3, 4, 5]
+print(reduce(lambda x, y : x * y, mult))
+
+# Questão 07
+frut = ["banana", "uva", "maçã", "laranja"]
+print(sorted(frut, key=lambda item : item [-1]))
