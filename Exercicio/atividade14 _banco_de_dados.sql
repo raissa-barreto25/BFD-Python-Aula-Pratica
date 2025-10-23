ecf8e7dba086f376f50f8a10347bebed857362e5
Raíssa Ruana <rrbsiilva@gmail.com>
2025-10-23 23:25:36 +0000

    atividade banco de dados

---
+++ Exercicio/atividade14 _banco_de_dados.sql
@@ -0,0 +1,22 @@
+-- Questão 01
+
+-- SELECT id_turma, COUNT () AS n_alunos
+-- FROM Aluno
+-- GROUP BY id_turma
+
+-- Questão 02
+-- SELECT *,  MIN (mensalidade) AS menor_mensalidade
+-- FROM Curso
+
+
+-- Questão 03
+-- SELECT nome , MAX (nota1) AS maior_nota1
+-- FROM Aluno
+
+-- Questão 04
+-- SELECT SUM (mensalidade)
+-- FROM Curso
+
+-- Questão 05
+-- SELECT AVG (nota2) AS media
+-- FROM Aluno
