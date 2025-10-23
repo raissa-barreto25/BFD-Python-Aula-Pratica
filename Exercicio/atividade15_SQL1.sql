e157a8b6e1b0a46f855364c42452e9e8b53e350c
Raíssa Ruana <rrbsiilva@gmail.com>
2025-10-23 23:40:03 +0000

    SQL

---
+++ Exercicio/atividade15_SQL1.sql
@@ -0,0 +1,25 @@
+-- Questão 01
+
+-- SELECT *
+-- FROM Aluno
+
+-- Questão 02
+
+-- SELECT nome, nota1
+-- FROM Aluno
+
+-- Questão 03
+
+-- SELECT nome, nota2
+-- FROM Aluno
+-- WHERE nota2 > 8
+
+-- Questão 04
+
+SELECT nome, data_nascimento
+FROM Aluno
+WHERE data_nascimento > 
+
+-- Questão 05
+
+
