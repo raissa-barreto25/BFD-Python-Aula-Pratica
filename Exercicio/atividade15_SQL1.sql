b761ea48d2916f45f27c66fe59e1e8f2895a9db3
Raíssa Ruana <rrbsiilva@gmail.com>
2025-10-26 14:52:55 +0000

    Exercicio SQL

--- Exercicio/atividade15_SQL1.sql
+++ Exercicio/atividade15_SQL1.sql
@@ -16,10 +16,43 @@
 
 -- Questão 04
 
-SELECT nome, data_nascimento
-FROM Aluno
-WHERE data_nascimento > 
+-- SELECT nome, data_nascimento
+-- FROM Aluno
+-- WHERE data_nascimento > '2000-01-01';
 
 -- Questão 05
 
+-- SELECT nome, mensalidade
+-- FROM Curso
+-- WHERE mensalidade >= 600;
 
+-- Questão 06
+
+-- SELECT nome, ano
+-- FROM Turma
+-- ORDER BY ano ASC;
+
+-- Questão 07
+
+-- SELECT ano, COUNT (*) AS quantidade_turmas
+-- FROM Turma
+-- GROUP BY ano;
+
+-- Questão 08
+
+-- SELECT id_turma, AVG (nota1) AS Media
+-- FROM Aluno
+-- GROUP BY id_turma;
+
+-- Questão 09
+
+-- SELECT ano, COUNT (*) as quantidade_turmas
+-- FROM Turma
+-- GROUP BY ano
+-- HAVING COUNT (*) > 2;
+
+-- Questão 10
+
+-- SELECT nome, mensalidade
+-- FROM Curso
+-- ORDER BY mensalidade DESC;
