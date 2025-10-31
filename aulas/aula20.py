cb06cbbcdc095ff95cbaab62ee000bd480d6a9d0
Raíssa Ruana <rrbsiilva@gmail.com>
2025-10-31 23:25:07 +0000

    mongo

--- aulas/aula20.py
+++ aulas/aula20.py
@@ -1,7 +1,8 @@
-# import pymongo
+import pymongo
 
 # cliente = pymongo.MongoClient("mongodb://localhost:27017/")
-# cliente = pymongo.MongoClient("mongodb+srv://rrbsiilva_db_user:BE8KKy8Oaqp6DFeW@cluster0.6alfy2x.mongodb.net/?appName=Cluster0")
+cliente = pymongo.MongoClient("mongodb+srv://rrbsiilva_db_user:BE8KKy8Oaqp6DFeW@cluster0.6alfy2x.mongodb.net/?appName=Cluster0")
+print(cliente)
 
 
 # lojinha = cliente["lojinhadb"]
@@ -17,6 +18,4 @@
 # #print(cliente.list_database_names())
 
 # #print(cliente)
-import pymongo
-
-cliente = pymongo.MongoClient("mongodb+srv://rrbsiilva_db_user:BE8KKy8Oaqp6DFeW@cluster0.6alfy2x.mongodb.net/?appName=Cluster0")
+# Comando - python aulas/aula20.py
